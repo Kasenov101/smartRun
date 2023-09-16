2a52146806cf7a5fd92c485c67acad6fed54de1d
Kasenov Abylay <kasenov101@gmail.com>
2023-09-16 15:10:51 +0600

    was adapted for mobile devices two last sections

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,18 +1,26 @@
 
  
 const sliderControl = () => {
-	const controlEl= document.getElementsByClassName("slider__control");
-	for (const control of controlEl) {
-		control.addEventListener("click", () =>  getAnotherImg(control));
+	let side;
+	if (screen.width >= 1200) {
+		const controlEl= document.getElementsByClassName("slider__control");
+		for (const control of controlEl) {
+			side = control.classList.contains("slide_left")? "left" : "right";
+			control.addEventListener("click", () =>  getAnotherImg(side));
+		}
+	} else {
+		const controlEl = document.getElementsByClassName("slider__wraper");
+		controlEl[0].addEventListener("touchstart", () => getAnotherImg("left"));
 	}
 }
 
-const getAnotherImg = (controlBtn) => {
+
+const getAnotherImg = (side, e) => {
 	const images = document.getElementsByClassName("slider__image");
 	let nextSlide;
 	for(let i = 0; i < images.length; i++) {
 		if (images[i].classList.contains("slide_vissible")) {
-			nextSlide = controlBtn.classList.contains("slide_left")? i - 1 : i + 1;
+			nextSlide = side === "left"? i - 1 : i + 1;
 			images[i].classList.remove("slide_vissible") 
 			break;
 		}
